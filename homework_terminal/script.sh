b1f8b5ee5e4aa941462871b7483e1f460fc18d18
nathaliavar <89571584+nathaliavar@users.noreply.github.com>
2022-05-01 15:57:31 +0300

    javascript tasks

--- homework_terminal/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd hw_dir/
-mkdir dir_1 dir_2 dir_3
-cd dir_1
-touch file_1.txt file_2.txt file_3.txt my_1.json my_2.json
-mkdir new_1 new_2 new_3
-ls -la
-mv file_1.txt file_2.txt new_2/
